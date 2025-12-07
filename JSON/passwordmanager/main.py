6f88888e0f05804b4b48be68147997c3c08ec589
Garrison White <garrison@2xys.com>
2025-12-07 16:42:40 -0500

    notetaker update

--- JSON/passwordmanager/main.py
+++ JSON/passwordmanager/main.py
@@ -12,12 +12,10 @@ def main():
         if choice == "1":
             user = login(data)
             if user:
-                note_taker(user)
+                note_taker(user, data)
         elif choice == "2":
             create_user(data)
         elif choice == "3":
-            change_info(data)
-        elif choice == "4":
             delete_user(data)
 
     
@@ -30,67 +28,91 @@ def main_menu():
         choice = input('''
 [1] Login
 [2] Create new user
-[3] Change username or password
-[4] Delete user
-[5] Quit \n>''')
+[3] Delete user
+[4] Quit \n>''')
         
     return choice
     
 
 def create_user(data):
     while True:
-        username = input("\nCreate username: ")
-        if username not in data.keys():
+        username = input("\nCreate username (Type 'back' to return to menu): ")
+        if username.lower() == "back":
+            break
+        elif username not in data.keys():
             break
         else:
             print("Sorry, that username already exists.")
             continue
     
-    pw = input("\nCreate password: ")
+    if username.lower() != "back":
+        pw = input("\nCreate password: ")
     
-    while True:
-        pw_check = input("Confirm password: ")
-        if pw_check != pw:
-            print("Invalid. Try again.")
-        else:
-            break
+        while True:
+            pw_check = input("Confirm password: ")
+            if pw_check != pw:
+                print("Invalid. Try again.")
+            else:
+                break
 
-    data[username] = pw
-    with open("data.json", "w") as f:
-        json.dump(data, f, indent=4)
+        data[0][username] = pw
+        data[1][username] = []
+        with open("data.json", "w") as f:
+            json.dump(data, f, indent=4)
 
 
 
 def login(data):
+    print("\n*Type 'back' to return to the menu*")
     while True:
         user_check = input("\nEnter username: ")
-        if user_check not in data.keys():
+        if user_check.lower() == "back":
+            break
+        elif user_check not in data[0].keys():
             print("User not found. Try again.")
         else:
             break
-
-    while True:
-        pw_check = input("\nEnter password: ")
-        if pw_check != data[user_check]:
-            print("\nIncorrect password. Try again.")
-        else: 
-            valid = True
-            break
-
-    if valid:
-        return user_check
-
-
-def change_info(data):
-    pass
+    if user_check.lower() != "back":
+        while True:
+            pw_check = input("\nEnter password: ")
+            if pw_check.lower() == "back":
+                break
+            elif pw_check != data[0][user_check]:
+                print("Incorrect password. Try again.")
+            else: 
+                return user_check
+                break
 
 
 def delete_user(data):
-    pass
+    print("\n*Type 'back' to return to the menu*")
+    while True:
+        user_check = input("\nEnter user to delete: ")
+        if user_check.lower() == "back":
+            break
+        elif user_check not in data[0].keys():
+            print("User not found. Try again.")
+        else:
+            break
+    if user_check.lower() != "back":
+        while True:
+            pw_check = input("\nEnter password: ")
+            if pw_check.lower() == "back":
+                break
+            elif pw_check != data[0][user_check]:
+                print("Incorrect password. Try again.")
+            else: 
+                choice = ""
+                while choice.lower() not in [""]
 
 
-def note_taker(user):
+def note_taker(user, data):
     print(input("\nUnder construction.\nPress Enter to return.\n"))
+    '''
+    1. Add a note
+    2. View all notes
+    3. Delete a note
+    4. Return to menu'''    
 
 
 if __name__ == "__main__":
